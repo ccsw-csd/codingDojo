496a631d4fc5d07d367f7de362da33bacb079f4e
javimolla <javier.molla.moreno@gmail.com>
2018-01-27 20:59:03 +0100

    Edge cases resolved

--- 05-KataPotter/elixir/javimolla/lib/potter.ex
+++ 05-KataPotter/elixir/javimolla/lib/potter.ex
@@ -2,7 +2,7 @@ defmodule Potter do
   @moduledoc """
   Documentation for Potter.
   """
-  
+
   def price([]) do
     0
   end 
@@ -14,23 +14,64 @@ defmodule Potter do
   def price(books) when is_list(books) do
     # Los agrupo según el libro
     # [0, 0, 1, 2, 2, 3] pasaría a ser [%{0 => [0, 0]}, %{1 => [1]} ...]
-    prepared = Enum.group_by(books, fn(book) -> book end)
-    |> Enum.map(fn(x) -> 
-      # Me quedo sólo con la parte de listado ya que se ha convertido en un map
-      # [%{0 => [0, 0]}, %{1 => [1]} ...] pasaría a ser [[0, 0], [1], ...]
-      elem(x, 1)
+    Enum.group_by(books, fn(book) -> book end)
+      |> Enum.map(fn(x) -> 
+        # Me quedo sólo con la parte de listado ya que se ha convertido en un map
+        # [%{0 => [0, 0]}, %{1 => [1]} ...] pasaría a ser [[0, 0], [1], ...]
+        elem(x, 1)
+      end)
+    |> length
+    |> get_lists(books)
+    |> Enum.filter(fn(x) ->
+      x != nil
     end)
+    |> Enum.map(fn(x) ->
+      pricep(x)
+    end)
+    |> Enum.min
+  end
 
-    # Obtenemos listados de libros 
-    # [[0, 0], [1], [2, 2], [3]] pasaría a ser [[0, 1, 2, 3], [0, 2]]
-    get_lists(prepared, 0, max_length(prepared))
+  defp get_lists(0, books) do
+    books
+  end
+  
+  defp get_lists(max_books, books) do
+    [get_list(max_books, books), get_list(max_books - 1, books)]
+  end
+
+  defp get_list(_, []) do
+    []
+  end
+
+  defp get_list(1, _) do
+    nil
+  end
+
+  defp get_list(max_books, books) do
+    uniques = books
+      |> Enum.uniq
+      |> Enum.slice(0..max_books-1)
+    rest = get_list(max_books, books -- uniques)
+    case length(rest) do
+      0 -> 
+        [uniques]
+      _ -> 
+        [uniques | rest]
+    end
+  end
+
+  defp pricep(books) do
+    books
     |> Enum.scan(0, fn(x, price) -> 
       y = Enum.filter(x, fn(x) -> x != nil end)
       num_books = length(y)
       # Aplicamos un descuento u otro dependiendo del número de libros distintos
       discount = 1 - (case num_books do
-        7 -> 0.55
-        _ -> (num_books - 1) * 0.05
+        2 -> 0.05
+        3 -> 0.1
+        4 -> 0.2
+        5 -> 0.25
+        _ -> 0
       end)
       price + num_books * 8 * discount
     end)
@@ -50,12 +91,5 @@ defmodule Potter do
         false -> nil
       end
     end) | get_lists(list, index + 1, max)]
-  end
-
-  defp max_length(list) do
-    list |> Enum.scan(0, fn(x, c) ->
-        if length(x) > c, do: length(x), else: c
-      end)
-      |> Enum.max
   end
 end
--- 05-KataPotter/elixir/javimolla/test/potter_test.exs
+++ 05-KataPotter/elixir/javimolla/test/potter_test.exs
@@ -12,28 +12,30 @@ defmodule PotterTest do
     assert 8 * 2 == Potter.price([0, 0])
     assert 8 * 3 == Potter.price([1, 1, 1])
   end
-# 
+   
   test "testSimpleDiscounts" do
     assert 8 * 2 * 0.95 == Potter.price([0, 1])
     assert 8 * 3 * 0.9 == Potter.price([0, 2, 4])
-    assert 8 * 4 * 0.85 == Potter.price([0, 1, 2, 4])
-    assert 8 * 5 * 0.8 == Potter.price([0, 1, 2, 3, 4])
+    assert 8 * 4 * 0.8 == Potter.price([0, 1, 2, 4])
+    assert 8 * 5 * 0.75 == Potter.price([0, 1, 2, 3, 4])
   end
   
   test "testSeveralDiscounts" do
     assert 8 + (8 * 2 * 0.95) == Potter.price([0, 0, 1])
     assert 2 * (8 * 2 * 0.95) == Potter.price([0, 0, 1, 1])
-    assert (8 * 4 * 0.85) + (8 * 2 * 0.95) == Potter.price([0, 0, 1, 2, 2, 3])
-    assert 8 + (8 * 5 * 0.8) == Potter.price([0, 1, 1, 2, 3, 4])
+    assert (8 * 4 * 0.8) + (8 * 2 * 0.95) == Potter.price([0, 0, 1, 2, 2, 3])
+    assert 8 + (8 * 5 * 0.75) == Potter.price([0, 1, 1, 2, 3, 4])
   end
   
-  # test "testEdgeCases" do
-  #   assert 2 * (8 * 4 * 0.85) == Potter.price([0, 0, 1, 1, 2, 2, 3, 4])
-  #   assert 3 * (8 * 5 * 0.8) + 2 * (8 * 4 * 0.8) == 
-  #     Potter.price([0, 0, 0, 0, 0, 
-  #          1, 1, 1, 1, 1, 
-  #          2, 2, 2, 2, 
-  #          3, 3, 3, 3, 3, 
-  #          4, 4, 4, 4])
-  # end
+  test "testEdgeCases" do
+    assert 2 * (8 * 4 * 0.8) == Potter.price([0, 0, 1, 1, 2, 2, 3, 4])
+    # Antes era 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8) pero eso es mayor 
+    # que 4 * (8 * 5 * 0.75) + (8 * 3 * 0.9) 
+    assert 4 * (8 * 5 * 0.75) + (8 * 3 * 0.9) == 
+      Potter.price([0, 0, 0, 0, 0, 
+           1, 1, 1, 1, 1, 
+           2, 2, 2, 2, 
+           3, 3, 3, 3, 3, 
+           4, 4, 4, 4])
+  end
 end
